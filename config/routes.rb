6cef46487bd57f8286a6bb1e3e7a93dddb848304
Barigakuruga <121374308+Barigakuruga@users.noreply.github.com>
2023-01-14 11:43:29 +0000

    [save]中間保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get 'books/show'
   get 'books/edit'
   get '/books' => 'books#index'
   post 'books' => 'books#create'
+  get '/books/:id' => 'books#show', as: 'book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
