c8ea89a953b2f5982c61973797b2cc42394b0ccd
Barigakuruga <121374308+Barigakuruga@users.noreply.github.com>
2023-01-15 12:26:35 +0000

    [save]中間保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
     flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
    else
+    @books = Book.all
     render :index
    end
   end
@@ -22,14 +23,17 @@ class BooksController < ApplicationController
   def edit
    @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+   else
+    render :edit 
+   end 
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
